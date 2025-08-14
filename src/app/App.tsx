7862a01b575fa2ad9d568a3aeab733970e62cda2
Ivan <ivonchik996@gmail.com>
2025-08-14 15:22:26 +0300

    fix App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,10 +5,8 @@ import {PostList} from "../widgets/PostList/PostList";
 
 export default function App() {
   return (
-    <>
       <MainLayout header={<Header />} footer={<Footer />}>
         <PostList />
       </MainLayout>
-    </>
   );
 }
